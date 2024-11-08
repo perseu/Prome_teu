ea94ecdd5db0ddf03c93d35b0df878b147e48f5b
perseu <jmsaguas@gmail.com>
2024-11-08 15:16:36 +0100

    Initial commit

--- prometeu.py
+++ prometeu.py
@@ -5,3 +5,19 @@ Created on Thu Nov  7 16:08:09 2024
 @author: JMSA
 """
 
+# The usual imports...
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+import yfinance as yf
+
+# Global variables
+ticker = ['GLD', 'SLV', 'CORN', 'WEAT']
+
+
+
+
+if __name__ == '__main__':
+    pass
